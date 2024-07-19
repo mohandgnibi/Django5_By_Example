01c4afe4e9c5a576ebf3827e2f601ea59dc86c3c
vostro <vostro@example.com>
2024-07-20 00:49:31 +0100

    Rendering forms in templates

--- Blog_Application/blog/views.py
+++ Blog_Application/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
 from .forms import EmailPostForm
@@ -51,17 +52,37 @@ def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
 
-    if request.method == "POST":
+    sent = False
+
+    if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']],
+            )
+            sent = True
     else:
         form = EmailPostForm()
     context = {
         "post": post,
         "form": form,
+        "sent": sent,
     }
     return render(request, "blog/post/share.html", context)
