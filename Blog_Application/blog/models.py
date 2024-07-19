3452aa6dda8605cab652441469479010cb7e86c6
vostro <vostro@example.com>
2024-07-19 22:26:15 +0100

    Adding pagination to the post list view

--- Blog_Application/blog/models.py
+++ Blog_Application/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -17,7 +18,7 @@ class Post(models.Model):
 
 
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE,
                                related_name='blog_posts',
@@ -42,4 +43,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail', 
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+                ]
+            )    
     
