63c163ddd100d37276b03b8d426a48d64a4dc285
vostro <vostro@example.com>
2024-07-19 01:11:14 +0100

    Chapter 01

--- Blog_Application/blog/models.py
+++ Blog_Application/blog/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -23,6 +30,9 @@ class Post(models.Model):
                               choices=Status,
                               default=Status.DRAFT
                               )
+    
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
