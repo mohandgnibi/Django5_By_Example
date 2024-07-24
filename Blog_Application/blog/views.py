911765206ef1a8bbfc8a983cf2b9dfd18c6e4c0a
vostro <vostro@example.com>
2024-07-24 20:44:18 +0100

    Start Chapter 3

--- Blog_Application/blog/views.py
+++ Blog_Application/blog/views.py
@@ -3,17 +3,23 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
+from taggit.models import Tag
 
 from .models import Post
 from .forms import EmailPostForm, CommentForm
 
+
 # Function-based views
-"""
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+    page_number = request.GET.get("page", 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
@@ -22,9 +28,11 @@ def post_list(request):
     except EmptyPage:
         # If page_number is out of range get last page of results
         posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
-"""
+    context = {
+        "posts": posts,
+        "tag": tag,
+    }
+    return render(request, "blog/post/list.html", context)
 
 
 # Class-based views
@@ -52,10 +60,18 @@ def post_detail(request, year, month, day, post):
     # Form for users to comment
     form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     context = {
         "post": post,
         "comments": comments,
         "form": form,
+        "similar_posts": similar_posts,
     }
     return render(request, "blog/post/detail.html", context)
 
--- Blog_Application/mysite/settings.py
+++ Blog_Application/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party apps.
+    "taggit",
     # My apps
     "blog",
 ]
