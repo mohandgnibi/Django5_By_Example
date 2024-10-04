85f58ad32991f382cb9a4f59bb9934e7d7d868eb
vostro <vostro@example.com>
2024-10-04 22:48:32 +0100

    Chapter05 <Running the development server through HTTPS>Change db to sqlite3

--- Social_Website/bookmarks/settings.py
+++ Social_Website/bookmarks/settings.py
@@ -81,13 +81,14 @@ WSGI_APPLICATION = "bookmarks.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 """
 DATABASES = {
     "default": {
@@ -99,6 +100,8 @@ DATABASES = {
         "PORT": 5432,  # default postgres port
     }
 }
+"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
