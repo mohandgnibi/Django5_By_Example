f9c0f520a931a154e5a697b06cdbe7a9058d870b
vostro <vostro@example.com>
2024-10-23 00:34:54 +0100

    Chapter06 <Adding infinite scroll pagination to the image list>

--- Social_Website/images/models.py
+++ Social_Website/images/models.py
@@ -36,4 +36,3 @@ class Image(models.Model):
 
     def get_absolute_url(self):
         return reverse("images:detail", args=[self.id, self.slug])
-    
--- Social_Website/images/views.py
+++ Social_Website/images/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -36,25 +38,54 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(
-        request,
-        "images/image/detail.html",
-        {"section": "images", "image": image}
+        request, "images/image/detail.html", {"section": "images", "image": image}
     )
 
 
 @login_required
 @require_POST
 def image_like(request):
-    image_id = request.POST.get('id')
-    action = request.POST.get('action')
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
-            if action == 'like':
+            if action == "like":
                 image.users_like.add(request.user)
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse({'status': 'ok'})
+            return JsonResponse({"status": "ok"})
         except Image.DoesNotExist:
             pass
-    return JsonResponse({'status': 'error'})
+    return JsonResponse({"status": "error"})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get("page")
+    images_only = request.GET.get("images_only")
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # If AJAX request and page out of range
+            # return an empty page
+            return HttpResponse("")
+        # If page out of range return last page of results
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            "images/image/list_images.html",
+            {"section": "images", "images": images},
+        )
+    return render(
+        request,
+        "images/image/list.html",
+        {"section": "images", "images": images},
+    )
