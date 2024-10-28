11513180704064204b5117afa8559e672f960ac1
vostro <vostro@example.com>
2024-10-28 19:36:33 +0100

    End Chapter07 <Counting image views with Redis>

--- Social_Website/images/views.py
+++ Social_Website/images/views.py
@@ -1,3 +1,5 @@
+import redis
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
@@ -10,6 +12,13 @@ from actions.utils import create_action
 
 from .forms import ImageCreateForm
 from .models import Image
+
+# Connect to redis
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
 
 
 @login_required
@@ -40,8 +49,18 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Increment total image views by 1
+    total_views = r.incr(f"image:{image.id}:views")
+    # Increment image ranking by 1
+    r.zincrby("image_ranking", 1, image.id)
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request, 
+        "images/image/detail.html", 
+        {
+            "section": "images", 
+            "image": image,
+            "total_views": total_views
+        }
     )
 
 
@@ -92,4 +111,26 @@ def image_list(request):
         request,
         "images/image/list.html",
         {"section": "images", "images": images},
+    )
+
+
+@login_required
+def image_ranking(request):
+    # Get image ranking dictionary
+    image_ranking = r.zrange(
+        "image_ranking", 0, -1,
+        desc=True
+    )[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Get most viewed images
+    most_viewed = list(
+        Image.objects.filter(
+            id__in=image_ranking_ids
+        )
+    )
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        "images/image/ranking.html",
+        {"section": "images", "most_viewed": most_viewed}
     )
