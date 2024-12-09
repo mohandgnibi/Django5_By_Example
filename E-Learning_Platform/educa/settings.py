02b952ecf69eaff7ec494cd2e0c1db376e8c1b8d
vostro <vostro@example.com>
2024-12-09 22:29:24 +0100

    Chapter14: Cache levels
    Using the low-level cache API
    Checking cache requests with Django Debug Toolbar
    Low-level caching based on dynamic data
    Caching template fragments
    Caching views
    Using the per-site cache

--- E-Learning_Platform/educa/settings.py
+++ E-Learning_Platform/educa/settings.py
@@ -45,12 +45,16 @@ INSTALLED_APPS = [
 
     # 3th party
     'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -138,7 +142,15 @@ LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
 CACHES = {
     'default': {
-        'BACKEND':'django.core.cache.backends.memcached.PyMemcachedCache',
+        'BACKEND':'django.core.cache.backends.memcached.PyMemcacheCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
+CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
