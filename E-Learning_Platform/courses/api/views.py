19c7478df3403bcd3ff4db8fe011cac4952b77f7
vostro <vostro@example.com>
2024-12-12 11:31:15 +0100

    Chapter15: Extending serializers
    Adding additional fields to serializers
    Implementing serializer method fields
    Adding pagination to views

--- E-Learning_Platform/courses/api/views.py
+++ E-Learning_Platform/courses/api/views.py
@@ -1,15 +1,18 @@
 from rest_framework import generics
+from django.db.models import Count
 
 from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
+from courses.api.pagination import StandardPagination
 
 
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     
