a04ee0f5291985f2465c51b5fa5b431fa9bcf7ee
vostro <vostro@example.com>
2024-12-14 12:53:41 +0100

    Chapter15: Building the course serializer
    Serializing relations
    Creating nested serializers
    Creating ViewSets and routers
    Building custom API views

--- E-Learning_Platform/courses/api/serializers.py
+++ E-Learning_Platform/courses/api/serializers.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from rest_framework import serializers
 
-from courses.models import Subject
+from courses.models import Course, Module, Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -19,4 +19,26 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
-        
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created',
+            'owner',
+            'modules',
+        ]
--- E-Learning_Platform/courses/api/views.py
+++ E-Learning_Platform/courses/api/views.py
@@ -1,18 +1,41 @@
 from rest_framework import generics
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
 from courses.api.pagination import StandardPagination
 
 
-class SubjectListView(generics.ListAPIView):
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+
+
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
     
