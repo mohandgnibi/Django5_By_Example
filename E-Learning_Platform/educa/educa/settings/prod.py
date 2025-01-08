5db2f097d971cb784bcc00dcb0a468fcd63e27cf
vostro <vostro@example.com>
2025-01-08 14:22:19 +0100

    Chapter17: Using Docker Compose
    Adding the Python requirements
    Configuring the PostgreSQL service
    Applying database migrations and creatings a superuser
    Configuring the Redis

--- E-Learning_Platform/educa/educa/settings/prod.py
+++ E-Learning_Platform/educa/educa/settings/prod.py
@@ -1,7 +1,8 @@
+from decouple import config
 from .base import *
 
 
-DEBUG = True
+DEBUG = False
 
 ADMINS = [
     ('Vostro', 'email@mydomain.com'),
@@ -11,7 +12,15 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME' : BASE_DIR/ 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
