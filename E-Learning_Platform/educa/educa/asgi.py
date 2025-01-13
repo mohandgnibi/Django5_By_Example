ffd1bace3a0a54ade594d48627aabd313cbeba9e
vostro <vostro@example.com>
2025-01-13 21:12:13 +0100

    Chapter17: Configuring Daphne for Django Channels
    Using secure connections for WebSockets
    Including Daphne in the NGINX configuration

--- E-Learning_Platform/educa/educa/asgi.py
+++ E-Learning_Platform/educa/educa/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
@@ -21,7 +22,9 @@ from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    )
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
 })
